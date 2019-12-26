17cc453972a21d3e09d225f7b8a67202860bd130
Robin Eklind <rnd0x00@gmail.com>
2019-12-26 23:51:20 +0100

    cmd/sgt: move generator to dedicated file

--- cmd/sgt/compile.go
+++ cmd/sgt/compile.go
@@ -3,44 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/llir/llvm/ir"
-	"github.com/llir/llvm/ir/constant"
-	"github.com/llir/llvm/ir/types"
 	"github.com/pkg/errors"
 	"golang.org/x/tools/go/ssa"
 )
-
-// generator is an LLVM IR generator for the given SSA Go package.
-type generator struct {
-	// Input Go package.
-	pkg *ssa.Package
-	// Output LLVM IR module.
-	module *ir.Module
-
-	// Map from constant name to LLVM IR constant.
-	consts map[string]constant.Constant
-	// Map from global name to LLVM IR global.
-	globals map[string]*ir.Global
-	// Map from function name to LLVM IR function.
-	funcs map[string]*ir.Func
-	// Map from type name to LLVM IR type.
-	types map[string]types.Type
-	// Map from predeclared type name to LLVM IR type.
-	predeclaredTypes map[string]types.Type
-}
-
-// newGenerator returns a new LLVM IR generator for the given SSA Go package.
-func newGenerator(pkg *ssa.Package) *generator {
-	return &generator{
-		pkg:              pkg,
-		module:           ir.NewModule(),
-		consts:           make(map[string]constant.Constant),
-		globals:          make(map[string]*ir.Global),
-		funcs:            make(map[string]*ir.Func),
-		types:            make(map[string]types.Type),
-		predeclaredTypes: make(map[string]types.Type),
-	}
-}
 
 // compileMember compiles the given SSA member into LLVM IR.
 func (gen *generator) compileMember(memberName string, member ssa.Member) error {
--- go.mod
+++ go.mod
@@ -3,15 +3,7 @@ module github.com/mewmew/skumgummitomte
 go 1.14
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/inspirer/textmapper v0.0.0-20190101225825-ae0b38e27780 // indirect
 	github.com/llir/llvm v0.3.0-pre7.0.20191226123353-433f268c0421
-	github.com/mewkiz/pkg v0.0.0-20181231041609-5720a0c5985d // indirect
-	github.com/mewspring/tools v0.0.0-20181204020634-6c6637dc82b6 // indirect
 	github.com/pkg/errors v0.8.1
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rickypai/natsort v0.0.0-20180124032556-f194e6bd5b0c // indirect
-	github.com/sergi/go-diff v1.0.0 // indirect
-	github.com/stretchr/testify v1.2.2 // indirect
 	golang.org/x/tools v0.0.0-20191224055732-dd894d0a8a40
 )
